ae76ce967226ec422f078fd9207917a1a8d0aa40
ankur singh <68726977+ankur2802@users.noreply.github.com>
2022-12-25 23:15:23 +0530

    Update Question_1.java

--- Question_1.java
+++ Question_1.java
@@ -46,52 +46,6 @@ public class Question_1{
 }
 
 
-//import java.util.*;
-//public class Question_1 {  
-//  public static void main(String[] args) {      
-//        int gf=0;
-//        int g=0;
-//        int j=0;
-//      //Initialize array   
-//     // int [] arr = new int []{1, 2, 3, 4, 2, 7, 8, 8, 3};  
-//  	boolean flag=false;
-//      Scanner sc= new Scanner(System.in); 
-//      System.out.println("enter the size of the array");
-//      int size=sc.nextInt();
-//      int arr[]=new int[size];
-//      System.out.println("enter values in array"); 
-//      
-//      // for storing value in array
-//      for (int k=0; k<arr.length; k++) {
-//      	arr[k]=sc.nextInt();
-//      }
-//       
-//      //Searches for duplicate element
-//      for(int i = 0; i < arr.length; i++) {  
-//          for( j = i+1; j <arr.length; j++) {  
-//              if(arr[i] == arr[j])  {
-//              	//System.out.println("Duplicate elements in given array: " + arr[j]);
-//                 // System.out.println(arr[j]);  
-//                // flag=true;
-//              	g++;
-//           
-//                  }
-//              else {
-//              	gf++;
-//              	//System.out.println("No duplicates at"+arr[j]);
-//              }
-//              
-//               }
-//          }
-//      if(g>0) {
-//      	System.out.println("Duplicate elements in given array: " + arr[j]);
-//      }
-//      else if(gf>=1) {
-//      	System.out.println("duplicate not found");
-//      }
-//      
-//      
-//      }  
-//  }  
+
 
 
